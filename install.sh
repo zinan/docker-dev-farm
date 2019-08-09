e5b58938f8191b2cc2951b09512ac99e205b0d80
Sinan TURGUT <mail@sinanturgut.com.tr>
2019-08-09 21:02:45 +0300

    Install script arg check added.

--- install.sh
+++ install.sh
@@ -19,38 +19,46 @@ else
 fi
 echo ""
 
-echo "Please enter projects directory path; will be used for web server configuration"
-echo "For example: /path/to/projects/directory"
-read DEFAULT_PROJECTS_FOLDER
-if [ -z "$DEFAULT_PROJECTS_FOLDER" ]
-then
-      echo "ERROR: Default Directory Path can not be NULL"
-      exit 1
+if [ -z $1 ]; then
+    echo "Please enter projects directory path; will be used for web server configuration"
+    echo "For example: /path/to/projects/directory"
+    read DEFAULT_PROJECTS_FOLDER
+    if [ -z "$DEFAULT_PROJECTS_FOLDER" ]
+    then
+          echo "ERROR: Default Directory Path can not be NULL"
+          exit 1
+    else
+        sed -i 's/^DEFAULT_PROJECTS_FOLDER=.*$/DEFAULT_PROJECTS_FOLDER='"$DEFAULT_PROJECTS_FOLDER"'/' .env
+        echo "Password written to .env"
+    fi
 else
-    sed -i 's/^DEFAULT_PROJECTS_FOLDER=.*$/DEFAULT_PROJECTS_FOLDER='"$DEFAULT_PROJECTS_FOLDER"'/' .env
-    echo "Password written to .env"
+    echo "Default Directory Path can not be NULL, Default value will be used..."
 fi
 echo ""
 
-echo "Please defina a GLOBAL PASSWORD which will be used for all necessary fields, like Mysql Root Password"
-read GLOBAL_PASSWORD
-if [ -z "$GLOBAL_PASSWORD" ]
-then
-      echo "GLOBAL PASSWORD can not be NULL, Default value will be used..."
+if [ -z $1 ]; then
+    echo "Please defina a GLOBAL PASSWORD which will be used for all necessary fields, like Mysql Root Password"
+    read GLOBAL_PASSWORD
+    if [ -z "$GLOBAL_PASSWORD" ]
+    then
+          echo "GLOBAL PASSWORD can not be NULL, Default value will be used..."
+    else
+        sed -i 's/^GLOBAL_PASSWORD=.*$/GLOBAL_PASSWORD='"$GLOBAL_PASSWORD"'/' .env
+        echo "Password written to .env"
+    fi
 else
-    sed -i 's/^GLOBAL_PASSWORD=.*$/GLOBAL_PASSWORD='"$GLOBAL_PASSWORD"'/' .env
-    echo "Password written to .env"
+    echo "GLOBAL PASSWORD can not be NULL, Default value will be used..."
 fi
 echo ""
 
 echo "Building and tagging Docker images..."
 echo ""
-docker-compose build
+#docker-compose build
 echo ""
 echo "Docker images built."
 
 echo "Cleaning up..."
-docker-compose down &> /dev/null
+#docker-compose down &> /dev/null
 
 echo ""
 echo "----------------"
