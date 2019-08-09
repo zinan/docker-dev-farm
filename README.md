29e1814ae9f7c96d077fc5d34c66f17a0475b6f1
Sinan TURGUT <mail@sinanturgut.com.tr>
2019-08-09 20:29:54 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# docker-dev-farm
-All in one dockerized development stack for especially Php developers
+# Docker Developer Farm
+
+All in one development stack for PHP developers
+
+### Requirements
+* Docker
+* Docker Compose
+
+### Setup
+Just clone or download repo and run **install.sh**
+
+If you need more tuning, please change default values in **.env** file
+
+
+### Supported Images
+- PHP 5.6
+- PHP 7.1
+- PHP 7.2
+- Nginx
+- Apache
+- Nodejs
+- Redis
+- Mongodb
+- Mysql
+- Redis Commander
+- PhpMyAdmin
+
+### Roadmap
+- Dbeaver CE image
+- PHP 7.3
+- ELK
+
+### Contributing
+This project is under constant development. Feedback and suggestions are
+welcome.
+
+While creating your pull request, please write a description which gives the context and/or explains why you are creating it.
+
+### License
+Licensed under the terms of the LGPL-3.0 license, see the [LICENSE](LICENSE) file.
---
+++ install.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Checking requirements..."
+echo ""
+
+docker info > /dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+  echo "ERROR: Unable to talk to the docker daemon"
+  echo ""
+  exit 1
+fi
+
+if [ -f ".env" ]; then
+  echo ".env already exists, skipped creation."
+else
+  echo "Creating .env"
+  cp -n .env.example .env
+fi
+echo ""
+
+echo "Please enter projects directory path; will be used for web server configuration"
+echo "For example: /path/to/projects/directory"
+read DEFAULT_PROJECTS_FOLDER
+if [ -z "$DEFAULT_PROJECTS_FOLDER" ]
+then
+      echo "ERROR: Default Directory Path can not be NULL"
+      exit 1
+else
+    sed -i 's/^DEFAULT_PROJECTS_FOLDER=.*$/DEFAULT_PROJECTS_FOLDER='"$DEFAULT_PROJECTS_FOLDER"'/' .env
+    echo "Password written to .env"
+fi
+echo ""
+
+echo "Please defina a GLOBAL PASSWORD which will be used for all necessary fields, like Mysql Root Password"
+read GLOBAL_PASSWORD
+if [ -z "$GLOBAL_PASSWORD" ]
+then
+      echo "GLOBAL PASSWORD can not be NULL, Default value will be used..."
+else
+    sed -i 's/^GLOBAL_PASSWORD=.*$/GLOBAL_PASSWORD='"$GLOBAL_PASSWORD"'/' .env
+    echo "Password written to .env"
+fi
+echo ""
+
+echo "Building and tagging Docker images..."
+echo ""
+docker-compose build
+echo ""
+echo "Docker images built."
+
+echo "Cleaning up..."
+docker-compose down &> /dev/null
+
+echo ""
+echo "----------------"
+echo "You're all done! Run the following command to get your docker developer farm running:"
+echo ""
+echo "  docker-compose up -d"
+echo ""
